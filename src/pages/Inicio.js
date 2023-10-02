0733829f5312654760f7422b48dc8472e55ac1a0
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2023-10-02 09:59:19 -0500

    DashBoard

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,11 +1,67 @@
 import React, { useState } from "react"
+import Alert from 'react-bootstrap/Alert'
 
 export default function Home(props){
-    let [authMode, setAuthMode] = useState("signin")
+  let [authMode, setAuthMode] = useState("signin")
   
-    const changeAuthMode = () => {
-      setAuthMode(authMode === "signin" ? "signup" : "signin")
+  const handleCloseAlert = () => {
+    setShowAlert(false);
+  };
+
+  const changeAuthMode = () => {
+    setAuthMode(authMode === "signin" ? "signup" : "signin")
+  }
+
+  const handleSuccess = () => {
+    setShowAlert(true);
+
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 2000); // 3000 milisegundos = 3 segundos
+  };
+
+  const [showAlert, setShowAlert] = useState(false);
+
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [rol, setRol] = useState('');
+  const [facultad, setFacultad] = useState('');
+  const [especialidad, setEspecialidad] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newUsers = {name,lastName,email,password,rol,facultad,especialidad};
+  
+    const headers = {
+      'Content-Type': 'application/json', // Especifica el tipo de contenido si es necesario,
+    };
+
+    console.log("Cuerpo del JSON:", JSON.stringify(newUsers));
+    console.log("Cabeceras:", headers);
+
+    try {
+      const response = await fetch('http://localhost:8080/users/newUser', {
+        method: 'POST',
+        body: JSON.stringify(newUsers),
+        headers: headers
+      })
+
+      if (response.ok) {
+        // La solicitud fue exitosa, puedes manejar la respuesta aquí
+        console.log('Solicitud POST exitosa');
+        handleSuccess();
+      } else {
+        // Manejar errores si la solicitud falla
+        console.error('Error en la solicitud POST');
+      }
+    } catch (error) {
+      console.error('Error en la solicitud POST:', error);
     }
+  };
+
   
     if (authMode === "signin") {
       return (
@@ -51,82 +107,107 @@ export default function Home(props){
   
     return (
       <div className="Auth-form-container">
-        <form className="Auth-form">
-          <div className="Auth-form-content">
-            <h3 className="Auth-form-title">Sign In</h3>
-            <div className="text-center">
-              No te has registrado aun ?{" "}
-              <span className="link-primary" onClick={changeAuthMode}>
-                Sign In
-              </span>
-            </div>
-            <div className="form-group mt-3">
-              <label>Nombres</label>
-              <input
-                type="firtsName"
-                className="form-control mt-1"
-                placeholder="Tus nombres"
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>Apellidos</label>
-              <input
-                type="lastName"
-                className="form-control mt-1"
-                placeholder="Tus Apellidos"
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>Email address</label>
-              <input
-                type="email"
-                className="form-control mt-1"
-                placeholder="Email Address"
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>Password</label>
-              <input
-                type="password"
-                className="form-control mt-1"
-                placeholder="Password"
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>Facultad</label>
-              <input
-                type="facultad"
-                className="form-control mt-1"
-                placeholder="Ingresa Tu facultad"
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>Rol</label>
-              <input
-                type="rol"
-                className="form-control mt-1"
-                placeholder="Ingresa tu rol"
-              />
-            </div>
-
-            <div className="form-group mt-3">
-              <label>especialidad</label>
-              <input
-                type="especialidad"
-                className="form-control mt-1"
-                placeholder="Que especialidad quieres ?"
-              />
-            </div>
-            <div className="d-grid gap-2 mt-3">
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-            </div>
-            <p className="text-center mt-2">
-              Forgot <a href="#">password?</a>
-            </p>
+      <form className="Auth-form" onSubmit={(e) => handleSubmit(e)}>
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign In</h3>
+          <div className="text-center">
+            Already registered?{" "}
+            <span className="link-primary" onClick={changeAuthMode}>
+              Sign In
+            </span>
           </div>
-        </form>
-      </div>
+          <div className="form-group mt-3">
+            <label>Nombres</label>
+            <input
+              type={"text"}
+              name="name"
+              value={name}
+              className="form-control mt-1"
+              placeholder="Pepito Pepo"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Apellidos</label>
+            <input
+              type={"text"}
+              name="lastName"
+              value={lastName}
+              className="form-control mt-1"
+              placeholder="Uribe Petro"
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <input
+              type={"text"}
+              name="email"
+              value={email}
+              className="form-control mt-1"
+              placeholder="Email Address"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type={"text"}
+              name="password"
+              value={password}
+              className="form-control mt-1"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Facultad</label>
+            <input
+              type={"text"}
+              name="facultad"
+              value={facultad}
+              className="form-control mt-1"
+              placeholder="Facultad"
+              onChange={(e) => setFacultad(e.target.value)}
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Rol</label>
+            <input
+              type={"text"}
+              name="rol"
+              value={rol}
+              className="form-control mt-1"
+              placeholder="Rol"
+              onChange={(e) => setRol(e.target.value)}
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Especialidad</label>
+            <input
+              type={"text"}
+              name="especialidad"
+              value={especialidad}
+              className="form-control mt-1"
+              placeholder="Especialidad"
+              onChange={(e) => setEspecialidad(e.target.value)}
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <p className="text-center mt-2">
+            Forgot <a href="#">password?</a>
+          </p>
+        </div>
+      </form>
+      {showAlert && (
+        <Alert variant="success" onClose={handleCloseAlert} dismissible>
+          La solicitud de creacion fue exitosa.
+        </Alert>
+      )}
+    </div>
     )
   }
--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,56 +1,63 @@
-import axios from 'axios'
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function register(props) {
   let [authMode, setAuthMode] = useState("signin")
 
-  let navigate = useNavigate()
 
   const changeAuthMode = () => {
     setAuthMode(authMode === "signin" ? "signup" : "signin")
   }
 
-  const [student, setStudent] = useState({
-    name: "",
-    lastName: "",
-    email: "",
-    password: "",
-    facultad: "",
-    rol: "",
-    especialidad: "",
-  })
-
-  const { name, lastName, email, password, facultad, rol, especialidad, } = student;
-
-  const onInputChange = (e) => {
-    setStudent({ ...user, [e.target.name]: e.target.value, [e.target.lastName]: e.target.value });
-   
-  };
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [rol, setRol] = useState('');
+  const [facultad, setFacultad] = useState('');
+  const [especialidad, setEspecialidad] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('lastname', lastName);
+    formData.append('email', email);
+    formData.append('password', password);
+    formData.append('rol', rol);
+    formData.append('facultad', facultad);
+    formData.append('especialidad', especialidad);
+
+    const headers = {
+      'Content-Type': 'application/json', // Especifica el tipo de contenido si es necesario
+    };
+
     try {
-      // Hacer la solicitud POST al endpoint
-      const response = await axios.post('http://localhost:8080/users/newUser', student);
+      const response = await fetch('http://localhost:8080/users/newUser', {
+        method: 'POST',
+        body: formData,
+        headers: headers
+      });
+      console.log(formData.values);
 
-      // Manejar la respuesta del servidor aquí, si es necesario
-      console.log('Respuesta del servidor:', response.data);
-
-      // Restablecer los campos del formulario después de enviarlos, si es necesario
-      e.target.reset();
-      navigate("/home");
+      if (response.ok) {
+        // La solicitud fue exitosa, puedes manejar la respuesta aquí
+        console.log('Solicitud POST exitosa');
+      } else {
+        // Manejar errores si la solicitud falla
+        console.error('Error en la solicitud POST');
+      }
     } catch (error) {
-      // Manejar errores de la solicitud aquí, si es necesario
-      console.error('Error al enviar la solicitud:', error);
+      console.error('Error en la solicitud POST:', error);
     }
   };
+
 
   if (authMode === "signin") {
     return (
       <div className="Auth-form-container">
-        <form className="Auth-form">
+        <form className="Auth-form" onSubmit={handleSubmit}>
           <div className="Auth-form-content">
             <h3 className="Auth-form-title">Sign In</h3>
             <div className="text-center">
@@ -108,7 +115,7 @@ export default function register(props) {
               value={name}
               className="form-control mt-1"
               placeholder="Pepito Pepo"
-              onChange={(e) => onInputChange(e)}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="form-group mt-3">
@@ -119,7 +126,7 @@ export default function register(props) {
               value={lastName}
               className="form-control mt-1"
               placeholder="Uribe Petro"
-              onChange={(e) => onInputChange(e)}
+              onChange={(e) => setLastName(e.target.value)}
             />
           </div>
           <div className="form-group mt-3">
@@ -130,7 +137,7 @@ export default function register(props) {
               value={email}
               className="form-control mt-1"
               placeholder="Email Address"
-              onChange={(e) => onInputChange(e)}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="form-group mt-3">
@@ -141,7 +148,7 @@ export default function register(props) {
               value={password}
               className="form-control mt-1"
               placeholder="Password"
-              onChange={(e) => onInputChange(e)}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <div className="form-group mt-3">
@@ -152,7 +159,7 @@ export default function register(props) {
               value={facultad}
               className="form-control mt-1"
               placeholder="Facultad"
-              onChange={(e) => onInputChange(e)}
+              onChange={(e) => setFacultad(e.target.value)}
             />
           </div>
           <div className="form-group mt-3">
@@ -163,7 +170,7 @@ export default function register(props) {
               value={rol}
               className="form-control mt-1"
               placeholder="Rol"
-              onChange={(e) => onInputChange(e)}
+              onChange={(e) => setRol(e.target.value)}
             />
           </div>
           <div className="form-group mt-3">
@@ -174,7 +181,7 @@ export default function register(props) {
               value={especialidad}
               className="form-control mt-1"
               placeholder="Especialidad"
-              onChange={(e) => onInputChange(e)}
+              onChange={(e) => setEspecialidad(e.target.value)}
             />
           </div>
           <div className="d-grid gap-2 mt-3">
